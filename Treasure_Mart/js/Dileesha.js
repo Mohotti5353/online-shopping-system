a49fd8c1dc73ef83363a0f005c9b08ed11fb8665
Mohotti5353 <168615486+Mohotti5353@users.noreply.github.com>
2025-03-18 11:34:41 +0530

    This is an online shopping mall system.

---
+++ Treasure_Mart/js/Dileesha.js
@@ -0,0 +1,48 @@
+function openConfirmationDialog() {
+    document.getElementById("confirmationDialog").style.display = "block";
+}
+
+function closeConfirmationDialog() {
+    document.getElementById("confirmationDialog").style.display = "none";
+}
+
+function deletecart(id) {
+    $.ajax({
+        url: window.location.href,
+        type: "POST",
+        data: { deleteCart: true, id: id },
+        success: function (response) {
+            response = response.trim();
+            if (response === "success") {
+                alert("Successfully Deleted the Item");
+                setTimeout("location.href = '../php/My%20Cart.php';", 400);
+            } else {
+                alert("SQL Error");
+            }
+        },
+        error: function () {
+            alert("Javascript Error");
+        }
+    });
+}
+
+function addtocart() {
+    var qty = document.getElementById("qty").value; 
+    $.ajax({
+        url: window.location.href,
+        type: "POST",
+        data: { addtoCart: true, qty: qty },
+        success: function(response) {
+            response = response.trim();
+            if (response === "success") {
+                alert("Successfully added to the cart");
+                setTimeout("location.href = '../php/Shop.php';",400);
+            } else {
+                alert("SQL Error");
+            }
+        },
+        error: function() {
+            alert("Javascript Error");
+        }
+    });
+}
